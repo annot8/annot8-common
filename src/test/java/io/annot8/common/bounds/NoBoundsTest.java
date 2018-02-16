9fe682fd03e3b89f9446f377c27835f79c19a0d2
James Baker <james.d.baker@gmail.com>
2018-02-16 14:46:29 +0000

    Fix failing tests for NoBounds

--- src/test/java/io/annot8/common/bounds/NoBoundsTest.java
+++ src/test/java/io/annot8/common/bounds/NoBoundsTest.java
@@ -4,7 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import io.annot8.core.data.Content;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,8 +20,12 @@ class NoBoundsTest {
     assertEquals(nb, NoBounds.getInstance());
     assertNotNull(nb.toString());
 
-    assertTrue(nb.isValid(null));
-    assertFalse(nb.getData(null, null).isPresent());
-    assertFalse(nb.getData(null).isPresent());
+    Content content = mock(Content.class);
+    when(content.getData()).thenReturn("Hello world!");
+
+    assertTrue(nb.isValid(content));
+    assertFalse(nb.getData(content, String.class).isPresent());
+    assertFalse(nb.getData(content, Long.class).isPresent());
+    assertFalse(nb.getData(content).isPresent());
   }
 }
