8bae416d5a177841a92f0872c84a9bbf0ae4862e
Steven Taylor <steven@committed.io>
2018-07-16 18:15:26 +0100

    Cleans up module-info file with simple open

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,32 +1,25 @@
 /**
- * This module contains common functionality for the Annot8 framework,
- * including helper and utility functions, abstract classes, factory
- * objects, and common implementations of some components (notably
- * Bounds).
+ * This module contains common functionality for the Annot8 framework, including helper and utility
+ * functions, abstract classes, factory objects, and common implementations of some components
+ * (notably Bounds).
  *
- * It does not contain default implementations of the majority of components,
- * which are held in the default-impl module.
+ * It does not contain default implementations of the majority of components, which are held in the
+ * default-impl module.
  *
- * The abstract classes in this module are there to provide correct
- * implementations of functions such as equals, hashCode and toString.
- * They do not provide any logic beyond this, and should generally be used
- * by any implementations of the interfaces they are abstracting.
+ * The abstract classes in this module are there to provide correct implementations of functions
+ * such as equals, hashCode and toString. They do not provide any logic beyond this, and should
+ * generally be used by any implementations of the interfaces they are abstracting.
  */
-module io.annot8.common {
+open module io.annot8.common {
   requires io.annot8.core;
-  
-  
-  opens io.annot8.common.annotations;
-  
-  // TODO Was exports not opens? But caused issues in tests
-  // See https://issues.apache.org/jira/browse/MCOMPILER-341
-  // Should be fixed in maven-compiler-plugin 3.7.1 (currently 3.7.0)
-  opens io.annot8.common.bounds;
-  opens io.annot8.common.content;
-  opens io.annot8.common.factories;
-  opens io.annot8.common.properties;
-  opens io.annot8.common.references;
-  opens io.annot8.common.registries;
-  opens io.annot8.common.stores;
-  opens io.annot8.common.utils;
+
+  exports io.annot8.common.annotations;
+  exports io.annot8.common.bounds;
+  exports io.annot8.common.content;
+  exports io.annot8.common.factories;
+  exports io.annot8.common.properties;
+  exports io.annot8.common.references;
+  exports io.annot8.common.registries;
+  exports io.annot8.common.stores;
+  exports io.annot8.common.utils;
 }
