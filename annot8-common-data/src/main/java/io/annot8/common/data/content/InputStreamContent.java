44ebd6ca89011c29d31a9904740335c087987d3c
Chris Flatley <chris@committed.io>
2018-08-22 09:25:02 +0200

    Add inputstreamcontent

---
+++ annot8-common-data/src/main/java/io/annot8/common/data/content/InputStreamContent.java
@@ -0,0 +1,16 @@
+package io.annot8.common.data.content;
+
+import io.annot8.core.data.Content;
+import java.io.File;
+import java.io.InputStream;
+
+
+/**
+ * Content which is backed by a input stream.
+ *
+ * Calling getData will re-open an inputstream each time (and the caller must close it themselves).
+ *
+ */
+public interface InputStreamContent extends Content<InputStream> {
+
+}
