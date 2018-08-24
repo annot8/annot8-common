e53ab6c4de751ea50df85749d559ffe03fa9596c
Steven Taylor <steven@committed.io>
2018-08-24 10:43:25 +0100

    Changes to Text implementation and adds further tests

--- annot8-common-data/src/main/java/io/annot8/common/data/content/Text.java
+++ annot8-common-data/src/main/java/io/annot8/common/data/content/Text.java
@@ -1,12 +1,12 @@
 package io.annot8.common.data.content;
 
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 import io.annot8.common.data.bounds.SpanBounds;
 import io.annot8.core.annotations.Annotation;
 import io.annot8.core.bounds.Bounds;
 import io.annot8.core.data.Content;
-import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 /**
  * Represents plain text content having data of type string.
@@ -37,6 +37,18 @@ public interface Text extends Content<String> {
    * @return annotations
    */
   default Stream<Annotation> getBetween(int begin, int end) {
+    SpanBounds offset = new SpanBounds(begin, end);
+    return filterAnnotations(SpanBounds.class, s -> offset.isWithin(s.getBegin(), s.getEnd()));
+  }
+
+  /**
+   * Get Annotations covering the provided offset
+   * 
+   * @param begin the start offset
+   * @param end the end offset
+   * @return annotations
+   */
+  default Stream<Annotation> getCovers(int begin, int end) {
     return filterAnnotations(SpanBounds.class, s -> s.isWithin(begin, end));
   }
 
@@ -56,13 +68,13 @@ public interface Text extends Content<String> {
    * @param offset offset
    * @return annotations
    */
-  default Stream<Annotation> getBefote(int offset) {
+  default Stream<Annotation> getBefore(int offset) {
     return filterAnnotations(SpanBounds.class, s -> s.isBefore(offset));
   }
 
-  private <T extends Bounds> Stream<Annotation> filterAnnotations(Class<T> boundsClass, Predicate<T> predicate) {
-    return getAnnotations()
-        .getByBounds(boundsClass)
+  private <T extends Bounds> Stream<Annotation> filterAnnotations(Class<T> boundsClass,
+      Predicate<T> predicate) {
+    return getAnnotations().getByBounds(boundsClass)
         .filter(a -> predicate.test(a.getBounds(boundsClass).get()));
   }
 }
