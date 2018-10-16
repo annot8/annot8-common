b06977159a5c0175a1f958b001318aade3d410bd
Chris Flatley <chris@committed.io>
2018-10-16 12:12:53 +0100

    Simplify creation of merge and branch with overloaded helpers

--- annot8-common-pipelines/src/main/java/io/annot8/common/pipelines/simple/SimpleBranchBuilder.java
+++ annot8-common-pipelines/src/main/java/io/annot8/common/pipelines/simple/SimpleBranchBuilder.java
@@ -1,15 +1,16 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.common.pipelines.simple;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
-import java.util.function.Supplier;
-
 import io.annot8.common.pipelines.definitions.BranchDefinition;
 import io.annot8.common.pipelines.elements.Branch;
 import io.annot8.common.pipelines.elements.BranchBuilder;
+import io.annot8.core.data.Item;
 import io.annot8.core.exceptions.IncompleteException;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class SimpleBranchBuilder implements BranchBuilder {
 
@@ -32,6 +33,11 @@ public class SimpleBranchBuilder implements BranchBuilder {
   @Override
   public BranchBuilder use(Supplier<Branch> branch) {
     supplier = branch;
+    return this;
+  }
+
+  public BranchBuilder use(Function<Item, String> decider) {
+    use(new SimpleBranch(decider));
     return this;
   }
 
