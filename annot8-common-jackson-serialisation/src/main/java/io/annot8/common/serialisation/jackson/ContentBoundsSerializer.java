3daa9cd87b5680cf2d55e7867002ad19729f0356
Chris Flatley <chris@committed.io>
2018-10-16 13:40:12 +0100

    Add serialisation support for contentbounds

---
+++ annot8-common-jackson-serialisation/src/main/java/io/annot8/common/serialisation/jackson/ContentBoundsSerializer.java
@@ -0,0 +1,21 @@
+/* Annot8 (annot8.io) - Licensed under Apache-2.0. */
+package io.annot8.common.serialisation.jackson;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import io.annot8.common.data.bounds.ContentBounds;
+import java.io.IOException;
+
+public class ContentBoundsSerializer extends AbstractAnnot8Serializer<ContentBounds> {
+
+  public ContentBoundsSerializer() {
+    super(ContentBounds.class);
+  }
+
+  @Override
+  public void serialize(ContentBounds value, JsonGenerator gen, SerializerProvider provider)
+      throws IOException {
+    gen.writeStartObject();
+    gen.writeEndObject();
+  }
+}
