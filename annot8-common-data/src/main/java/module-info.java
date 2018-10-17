5b93698e3fbe9592237ba1fd84a35648941f0900
Steven Taylor <steven@committed.io>
2018-10-17 08:38:48 +0100

    Adds a Table data type with Content and Bounds

--- annot8-common-data/src/main/java/module-info.java
+++ annot8-common-data/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 open module io.annot8.common.data {
   requires transitive io.annot8.core;
+  requires io.annot8.common.utils;
 
   exports io.annot8.common.data.bounds;
   exports io.annot8.common.data.content;
